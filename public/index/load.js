014f72f353159e6f2efa0306ac65422cd042c06a
FENG Jiabin <benplusplus@gmail.com>
2016-07-05 15:01:48 +0800

    Initial implementation

---
+++ public/index/load.js
@@ -0,0 +1,9 @@
+(function () {
+
+  // var el = document.querySelector('#foo:bar');
+
+  document.addEventListener('DOMContentLoaded', function(){
+    console.log("DOM Loaded");
+  }, false);
+
+})();
